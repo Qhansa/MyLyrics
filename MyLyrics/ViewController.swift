8a644aa1a9937659f54f799a3836ad2ae71e2f9b
Qhansa <qhansa_dp@hotmail.com>
2022-05-08 18:13:57 +0700

    Initial Commit

---
+++ MyLyrics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLyrics
+//
+//  Created by Qhansa D. Bayu on 08/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
